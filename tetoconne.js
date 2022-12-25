224526a32112067b2009aa41970168fb7f656621
3-show <120150062+3-show@users.noreply.github.com>
2022-12-25 21:12:04 +0900

    Update tetoconne.js

--- tetoconne.js
+++ tetoconne.js
@@ -1,21 +1,122 @@
-const header = document.querySelector('header');
-const section = document.querySelector('section');
+class CSV {
+  constructor(data, keys = false) {
+    this.ARRAY = Symbol('ARRAY');
+    this.OBJECT = Symbol('OBJECT');
 
-let requestURL = 'https://mypage2.tetoteconnect.jp/mypage-web2/api/user?data=degrees&lang=ja_jp';
+    this.data = data;
 
-let request = new XMLHttpRequest();
+    if (CSV.isArray(data)) {
+      if (0 == data.length) {
+        this.dataType = this.ARRAY;
+      } else if (CSV.isObject(data[0])) {
+        this.dataType = this.OBJECT;
+      } else if (CSV.isArray(data[0])) {
+        this.dataType = this.ARRAY;
+      } else {
+        throw Error('Error: 未対応のデータ型です');
+      }
+    } else {
+      throw Error('Error: 未対応のデータ型です');
+    }
 
-request.open('GET', requestURL);
+    this.keys = keys;
+  }
 
-request.responseType = 'json';
-request.send();
+  toString() {
+    if (this.dataType === this.ARRAY) {
+      return this.data
+        .map((record) => record.map((field) => CSV.prepare(field)).join(','))
+        .join('\n');
+    } else if (this.dataType === this.OBJECT) {
+      const keys = this.keys || Array.from(this.extractKeys(this.data));
 
-request.onload = function() {
-  const userData = request.response;
-  testAlert(userData);
+      const arrayData = this.data.map((record) =>
+        keys.map((key) => record[key])
+      );
+
+      return []
+        .concat([keys], arrayData)
+        .map((record) => record.map((field) => CSV.prepare(field)).join(','))
+        .join('\n');
+    }
+  }
+
+  save(filename = 'data.csv') {
+    if (!filename.match(/\.csv$/i)) {
+      filename = filename + '.csv';
+    }
+
+    const csvStr = this.toString();
+
+    const bom = new Uint8Array([0xef, 0xbb, 0xbf]);
+    const blob = new Blob([bom, csvStr], {
+      type: 'text/csv'
+    });
+    const url = window.URL || window.webkitURL;
+    const blobURL = url.createObjectURL(blob);
+
+    let a = document.createElement('a');
+    a.download = decodeURI(filename);
+    a.href = blobURL;
+    a.type = 'text/csv';
+
+    a.click();
+  }
+
+  extractKeys(data) {
+    return new Set(
+      [].concat(...this.data.map((record) => Object.keys(record)))
+    );
+  }
+
+  static prepare(field) {
+    return '"' + ('' + field).replace(/"/g, '""') + '"';
+  }
+
+  static isObject(obj) {
+    return '[object Object]' === Object.prototype.toString.call(obj);
+  }
+
+  static isArray(obj) {
+    return '[object Array]' === Object.prototype.toString.call(obj);
+  }
 }
 
-function testAlert(obj) {
-  const myH1 = obj['response']['name'] + ' のコネクトレベル：' + obj['response']['level'];
-  alert(myH1);
+try {
+	const URL = location.href;
+	
+	if (URL != 'https://mypage2.tetoteconnect.jp/mypage-web2/?news=1') {
+		throw new Error(
+			'表示しているページが違います。マイページのトップを開いて再度試してください。'
+		);
+	}
+	
+	let requestURL = 'https://mypage2.tetoteconnect.jp/mypage-web2/api/user?data=degrees&lang=ja_jp';
+	let request = new XMLHttpRequest();
+	
+	request.open('GET', requestURL);
+	request.responseType = 'json';
+	request.send();
+	
+	request.onload = function() {
+		const userData = request.response;
+		makeCSV(userData);
+		
+		alert(
+			`テトコネスコア集計ツール\nボタンを押すとCSVのダウンロードが始まります。`
+		);
+	}
+} catch (e) {
+	alert(e);
+}
+
+function makeCSV(obj) {
+	let results = [];
+	let tmpResult;
+	
+	tmpResult = {
+		title: obj['response']['level']
+	};
+	
+	results.push(tmpResult);
 }
